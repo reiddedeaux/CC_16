3a62e9ab9f7829eba22a9170ea26b4e74f3e3538
reiddedeaux <rdedeaux@usf.edu>
2025-03-30 12:07:44 -0400

    Task 3 - Add fetchProductsAsync using async/await and try/catch

--- CC_16.js
+++ CC_16.js
@@ -20,3 +20,16 @@ function fetchProductsThen(){
         console.error(`There is a problem with fethcing this`,error); //logs the error into the console
     })
 }
+//Task 3 
+async function fetchProductsAsync(){
+    try{
+        const response = await fetch(productUrl); //fetches products information using await
+
+        const products = await response.json();//changes to json
+
+        displayProducts(products);//shows the products
+    }
+    catch(error){
+        handleError(error);//checks to see if theres an error
+    }
+}
