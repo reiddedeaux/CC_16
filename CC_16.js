9530163349af60057a96738ec7df8177f4e52402
reiddedeaux <rdedeaux@usf.edu>
2025-03-30 12:30:34 -0400

    Task 4 - Display products in the DOM with name, price, and image

--- CC_16.js
+++ CC_16.js
@@ -33,3 +33,33 @@ async function fetchProductsAsync(){
         handleError(error);//checks to see if theres an error
     }
 }
+//Task 4 
+function displayProducts(products){
+    let divProductContainer = document.getElementById(`product-container`);
+
+    products.slice(0, 5).forEach(product =>{ //first 5 products
+        const productCard = document.createElement(`div`);
+        productCard.setAttribute(`class`, `product-card`);
+        //create and add product name
+        const productName = document.createElement(`h3`);
+        productName.setAttribute(`class`, `product-header`);
+        productName.textContent = product.fields.name; 
+        productCard.append(productName);
+        //create and add price
+        const productPrice = document.createElement(`div`);
+        productPrice.setAttribute(`class`, `product-price`);
+        productName.textContent = `$` + product.fields.price;
+        productCard.append(productPrice);
+        //create and add image
+        const divProductImage = document.createElement(`div`);
+        divProductImage.setAttribute(`class`, `product-image`);
+        const productImage = document.createElement(`img`);
+        productImage.src = product.fields.image[0].thumbnails.small.url;
+        productImage.width = 215;
+        productImage.height = 215;
+        divProductImage.append(productImage);
+        productCard.append(divProductImage);
+        //append the product card to main container
+        divProductContainer.append(productCard);
+    })
+}
